ac4d9e5509cd828b6536e4cb8595f7fde7d2e6e1
OrdinaryFabulous <144396231+OrdinaryFabulous@users.noreply.github.com>
2023-11-29 13:59:09 -0500

    Template For Saves And Dungeons

--- Main.py
+++
@@ -1,8 +0,0 @@
-import DialogueService, SaveService, EventService, SequenceService, time
-
-def Main():
-    SaveID = SequenceService.MainMenu()
-    print(SaveService.GetSave(SaveID))
-    SequenceService.tutorial()
-
-Main()
---
+++ bleh.py
@@ -0,0 +1,7 @@
+dice = {
+    "yes": "yes",
+    "yes2": "No"
+}
+
+print(dice.get("No", None))
+
