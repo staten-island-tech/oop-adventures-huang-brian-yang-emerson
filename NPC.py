4d13ea7cc3f401f33e6328a270a166a4391174b7
brianh181 <144480654+brianh181@users.noreply.github.com>
2023-12-04 14:01:43 -0500

    meh

--- NPC.py
+++ NPC.py
@@ -3,7 +3,11 @@ class Shop:
         self.name = name
         self.items = items
         self.costs = costs
-    def intro(self,name):
-        print("Hey, it's ya boi! Care to look into my inventory?")
+    def intro(name):
+        print("Hey, it's ya boi "+name+"! Care to look into my inventory?")
+    def sell(items,costs):
+        print("Hey~~~ o///v///o")
 
 Emerson = Shop("Emerson",["Apple","Banana"],[5,5])
+
+Shop.intro(Emerson.name)
