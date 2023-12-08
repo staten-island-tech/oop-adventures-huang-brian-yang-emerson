a770cf2e87b7a7b8c54a5b15aec4464fc0f4d09f
brianh181 <144480654+brianh181@users.noreply.github.com>
2023-12-08 13:59:44 -0500

    e

--- Map.py
+++ Map.py
@@ -3,11 +3,34 @@ import os
 class Map:
     def __init__(self,map):
         self.map = map
-    def PrintMap(self,map):
+    def PrintMap(self):
         os.system("cls")
+        for i in self.map:
+            print(''.join(i))
+    def Move(self):
+        movement = input().upper
+        if movement == "W":
+            y = xy[1]
+            xy[1] = y - 1
+        if movement == "S":
+            y = xy[1]
+            xy[1] = y + 1
+        if movement == "D":
+            xy[0] += 1
+        if movement == "A":
+            xy[0] -= 1
+        return xy
+        
 
-        for i in range(map):
-            for e in range(map[i]):
-                print(map[i][e])
-    
-e = Map([[0,0,0,0,0]])
+xy = [0,0]                
+
+for i in range(5):
+    x = xy[0]
+    y = xy[1]
+
+    Template = [["#" for i in range(5)] for i in range(5)]
+    Template[x][y] = "P"
+
+    e = Map(Template)
+    e.PrintMap()
+    xy = e.Move()
