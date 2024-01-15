7b6f42277b7e5461a41719f65211c410794bae3c
OrdinaryFabulous <144396231+OrdinaryFabulous@users.noreply.github.com>
2024-01-14 21:17:34 -0500

    s
    s

--- UselessOrWhateverFile/Whop.py
+++ UselessOrWhateverFile/Whop.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, time
 
 def Answer(Options: list):
 	Confirmaton = input(f"{'/'.join(Options)}: ").upper()
@@ -28,41 +28,38 @@ def CoolBoxDialogue(ListOfDialogue: list[str], AvailableActions: list[str], Acti
 
 
 def Guard():
-    os.system('cls')
-
-    current_selection = 0
-    PlayerLevel = 1  # This will be replaced with the actual player level
-
-    with open('DData.json', mode='r') as infile:
-        Dungeons: list[dict] = json.load(infile)
-
-    while True:
-        os.system('cls')
-        dialogues = ["Select Dungeon:"]
-
-        for i, dungeon in enumerate(Dungeons):
-
-            if PlayerLevel >= dungeon['LevelReq']:
-                dialogue = f"Dungeon {i}: {dungeon['Dungeon']}"
-            else:
-                dialogue = f"Dungeon {i}: {dungeon['Dungeon']} (Locked!)"
-
-            if i == current_selection and PlayerLevel >= dungeon['LevelReq']:
-                dialogue += ' <--'
-
-            dialogues.append(dialogue)
-
-        actions = ["W/S - Move Up/Down Respectively", "P - Select Dungeon", "R - Return to Dungeon Screen"]
-        answers = ["W", "S", "P", "R"]
-
-        DungeonChoice = CoolBoxDialogue(dialogues, actions, answers, 80)
-
-        if DungeonChoice == 0 and current_selection > 0:
-            if PlayerLevel >= Dungeons[current_selection - 1]['LevelReq']:
-                current_selection -= 1
-
-        elif DungeonChoice == 1 and current_selection < len(Dungeons) - 1:
-            if PlayerLevel >= Dungeons[current_selection + 1]['LevelReq']:
-                current_selection += 1
+	os.system('cls')
+	current_selection = 0
+	PlayerLevel = 1  # This will be replaced with the actual player level
+	with open('DData.json', mode='r') as infile:
+		Dungeons: list[dict] = json.load(infile)
+	while True:
+		os.system('cls')
+		dialogues = ["Select Dungeon:"]
+		for i, dungeon in enumerate(Dungeons):
+			if PlayerLevel >= dungeon['LevelReq']:
+				dialogue = f"Dungeon {i}: {dungeon['Dungeon']}"
+			else:
+				print("Not Enough Level")
+				dialogue = f"Dungeon {i}: {dungeon['Dungeon']} (Locked!)"
+			if i == current_selection and PlayerLevel >= dungeon['LevelReq']:
+				dialogue += ' <--'
+			dialogues.append(dialogue)
+		actions = ["W/S - Move Up/Down Respectively", "P - Select Dungeon", "R - Return"]
+		answers = ["W", "S", "P", "R"]
+		DungeonChoice = CoolBoxDialogue(dialogues, actions, answers, 80)
+		if DungeonChoice == 0 and current_selection > 0:
+			if PlayerLevel >= Dungeons[current_selection - 1]['LevelReq']:
+				current_selection -= 1
+		elif DungeonChoice == 1 and current_selection < len(Dungeons) - 1:
+			if PlayerLevel >= Dungeons[current_selection + 1]['LevelReq']:
+				current_selection += 1
+		
+		elif DungeonChoice == 2:
+			pass
+		else:
+			print("Exiting!!")
+			time.sleep(5)
+			return "GET OUT OF MY SKIN"
 
 Guard()
